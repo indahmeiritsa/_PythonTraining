d67cd0de491960d806f7c4bdc62f506420272112
unknown <indah.meiritsa@gmail.com>
2022-06-10 21:00:40 +0700

    intial commit add sesi 1

---
+++ contoh.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#contoh1
+print(123123+1)
+
+#contoh2
+print(10)
+print(type(10))
+
+#contoh3
+print(4.2)
+print(type(4.2))
+
+#contoh4
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+#contoh5
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+#contoh6
+print(type(True))
+print(type(False))
+
+#contoh7
+n=300
+print(n)
+
+#contoh latihan luas segitiga
+a=10
+t=20
+print(0.5*a*t)
+
+#contoh8
+a = b = c = 300
+print(a, b, c)
+
+#contoh9
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+#contoh10
+name = "Hacktiv8" #karakter harus menggunakan kutip "
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
+
+#contoh11
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+
+print(age, Age, aGe, AGE, a_g_e, _age, age_, _AGE_)
+
+#contoh12
+a = 10
+b = 20
+print(a + b)
+
+#contoh13
+a = 10
+b = 20
+a + b - 5
+
+print(a + b * 5)
+
+#contoh13
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+#contoh14
+a = 10
+b = 20
+print(a == b)
+
+print(a != b)
+
+print(a <= b)
+
+print(a >= b)
+
+a = 30
+b = 30
+print(a == b)
+
+print(a <= b)
+
+print(a >= b)
+
+#contoh15
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
+
+
+
+
+
+
+
+
---
+++ training1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print(123123+1)
+
+print(10)
+print(type(10))
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+print(type(True))
+print(type(False))
